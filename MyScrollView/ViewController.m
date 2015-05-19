6887e46523dfffe6d3a3ab908b7b4cbab0c0f70a
Jacy Wang <jacywj@gmail.com>
2015-05-19 09:24:08 -0700

    Add four views

--- MyScrollView/ViewController.m
+++ MyScrollView/ViewController.m
@@ -17,6 +17,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
+    redView.backgroundColor = [UIColor redColor];
+    UIView *greenView = [[UIView alloc] initWithFrame:CGRectMake(150, 150, 150, 200)];
+    greenView.backgroundColor = [UIColor greenColor];
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(40, 400, 200, 150)];
+    blueView.backgroundColor = [UIColor blueColor];
+    UIView *yellowView = [[UIView alloc] initWithFrame:CGRectMake(100, 600, 180, 150)];
+    yellowView.backgroundColor = [UIColor yellowColor];
+    
+    [self.view addSubview:redView];
+    [self.view addSubview:greenView];
+    [self.view addSubview:blueView];
+    [self.view addSubview:yellowView];
 }
 
 - (void)didReceiveMemoryWarning {
